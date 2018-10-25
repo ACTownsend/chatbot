2f3a60b9ed376074d7b9f29e703001f97bbbd292
Adam Townsend <townse42@coventry.ac.uk>
2018-10-25 17:57:35 +0000

    first interaction

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,22 @@
-h
+from random import *
+print(chr(27) + "[2J")
+greetings = ["Hello", "Hi", "Hey", "Yo"]
+
+def greet(user):
+    randomGreet = choice(greetings)
+    return randomGreet
+
+user = input("Welcome to chatbot! How may I help you?\n>>> ")
+user = user.capitalize()
+
+if user in greetings:
+    resp = greet(user)
+    print(resp + "! What can I do for you?\n>>> ")
+else:
+    print("Sorry I didn't quite catch that\n")
+
+
+    
+   
+
+    
