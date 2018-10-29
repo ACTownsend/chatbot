714c2e6ac07830f8576bfce192a43dd30342f09b
Adam Townsend <townse42@coventry.ac.uk>
2018-10-29 16:34:21 +0000

    Added an exit to the program

--- chatbot.py
+++ chatbot.py
@@ -7,19 +7,23 @@ print(chr(27) + "[2J")
 
 user = input(" Bot: Welcome to chatbot! How may I help you?\n User: ")
 user = user.lower()
-while user != "go away":
+exit = False
+while exit == False:
     user = user.split(" ")
 
     for i in user:
         if i in greetings:
             resp = greet(user)
             user = input(" Bot: " + resp + "! What can I do for you?\n User: ")
+        elif i in farewell:
+            resp = endFunction(user)
+            user = print(" Bot: " + resp + "!") 
+            exit = True
         else:
-            user = input(" Bot: Sorry I didn't quite catch that!\n User: ")
+            a = input(" Bot: Sorry I didn't quite catch that!\n User: ")
 
-print("Fine then....")
+
 
     
-   
 
     
