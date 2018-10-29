76e79724026921c21b75a81f4baea5766af60e49
Adam Townsend <townse42@coventry.ac.uk>
2018-10-29 09:55:04 +0000

    updated the bot so it loops

--- chatbot.py
+++ chatbot.py
@@ -6,14 +6,18 @@ print(chr(27) + "[2J")
 
 
 user = input(" Bot: Welcome to chatbot! How may I help you?\n User: ")
-user = user.capitalize()
+user = user.lower()
+while user != "go away":
+    user = user.split(" ")
 
-if user in greetings:
-    resp = greet()
-    print(" Bot: " + resp + "! What can I do for you?\n ")
-else:
-    print(" Bot: Sorry I didn't quite catch that\n")
+    for i in user:
+        if i in greetings:
+            resp = greet(user)
+            user = input(" Bot: " + resp + "! What can I do for you?\n User: ")
+        else:
+            user = input(" Bot: Sorry I didn't quite catch that!\n User: ")
 
+print("Fine then....")
 
     
    
