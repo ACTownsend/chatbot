2ffdf3ffd6f71656be15fc103e3b94af35f9d46c
Adam Townsend <townse42@coventry.ac.uk>
2018-10-29 16:35:07 +0000

    Added a list of ways to bid the user farewell

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 from random import *
 greetings = ["hello", "hi", "hey", "yo"]
-
+farewell = ["goodbye", "cya later", "bye", "cya", "later"]
 def greet(user):
     randomGreet = choice(greetings).capitalize()
     return randomGreet
+
+def endFunction(user):
+    randomBye = choice(farewell).capitalize()
+    return randomBye
