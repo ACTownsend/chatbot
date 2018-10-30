0db12085e0a13541aee344fe59bb4c86529fcb96
Adam Townsend <townse42@coventry.ac.uk>
2018-10-30 10:41:17 +0000

    cleaned up the code and fixed some bugs

--- chatbot.py
+++ chatbot.py
@@ -8,19 +8,32 @@ print(chr(27) + "[2J")
 user = input(" Bot: Welcome to chatbot! How may I help you?\n User: ")
 user = user.lower()
 exit = False
+ran = 0
+should = 0 
 while exit == False:
-    user = user.split(" ")
-
+    user = user.split(" ")    
     for i in user:
-        if i in greetings:
-            resp = greet(user)
-            user = input(" Bot: " + resp + "! What can I do for you?\n User: ")
-        elif i in farewell:
-            resp = endFunction(user)
-            user = print(" Bot: " + resp + "!") 
-            exit = True
-        else:
-            a = input(" Bot: Sorry I didn't quite catch that!\n User: ")
+        should += 1
+        for j in greetings:
+            if i == j:
+                resp = greet(user)
+                user = input(" Bot: " + resp + "! What can I do for you?\n User: ")
+                ran = should
+        for j in farewell:
+            if i == j:
+                resp = endFunction(user)
+                user = print(" Bot: " + resp + "!") 
+                exit = True
+                ran = should
+        for j in browser:
+            if i == j:
+                googleSearch()
+                ran = should 
+                user = input(" Bot: Anything else I can help you with?\n User: ")
+                
+    if ran != should:
+        user = input(" Bot: Sorry, I didnt quite catch that\n User: ")
+
 
 
 
