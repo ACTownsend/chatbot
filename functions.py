205417fd4ad52101a3ee25fc3af8ea28ca6af24a
Adam Townsend <townse42@coventry.ac.uk>
2018-10-30 10:41:57 +0000

    Added the google search function

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 from random import *
+import webbrowser
+
 greetings = ["hello", "hi", "hey", "yo"]
 farewell = ["goodbye", "cya later", "bye", "cya", "later"]
+browser = ["google", "search"]
+
 def greet(user):
     randomGreet = choice(greetings).capitalize()
     return randomGreet
@@ -8,3 +12,13 @@ def greet(user):
 def endFunction(user):
     randomBye = choice(farewell).capitalize()
     return randomBye
+
+def googleSearch():
+    address = 'http://www.google.com/#q='
+    search = input(" Bot : What would you like to search?\n User: ")
+    searchterm = address + search
+    print("Here is a link for: " + searchterm)
+    webbrowser.open_new(searchterm)
+    return searchterm
+    
+    
